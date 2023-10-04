e0611962c3a02316b38798ad9548a5d09bb7857c
bajrangCoder <nkray21111983@gmail.com>
2023-10-05 03:53:59 +0530

    Fixed deloy issue

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Acode PLugin Docs',
-  tagline: 'A community made acode plugin documentation',
+  tagline: 'A community driven acode plugin documentation',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
